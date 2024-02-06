717c9e0f09136c9a6e1d2a0fb90e2399ee9a235c
Chestna <chestna.mitchell@outlook.com>
2024-02-06 11:50:02 -0500

    technicians api: making progress

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+
+from .models import Technician
+from .encoders import (
+	TechnicianEncoder,
+)
+
 
 # Create your views here.
+@require_http_methods(["GET","POST"])
+def api_technicians(request):
+    if request.method == "GET":
+        technicians = Technician.objects.all()
+        return JsonResponse(
+            {"technicians": technicians}
+            encoder=TechnicianEncoder,
+        )
+    else:
+        try:
+            content = json.loads(request.body)
+            technician = Technician.objects.create(**content)
+            return JsonResponse(
+                technician,
+                encoder=TechnicianEncoder,
+                safe=False,
+            )
+        except:
+            response = JsonResponse(
+                {"message": "Could not add technician"}
+            )
+            response.status_code = 400
+            return response
