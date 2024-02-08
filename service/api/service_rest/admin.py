cb2b3c9c75ee5731d2f5849529a49d7e3c4eba26
Chestna <chestna.mitchell@outlook.com>
2024-02-08 11:49:19 -0500

    imports

--- service/api/service_rest/admin.py
+++ service/api/service_rest/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from service_rest.models import Appointment, AutomobileVO, Technician
+
 # Register your models here.
 admin.site.register(AutomobileVO)
 admin.site.register(Technician)
