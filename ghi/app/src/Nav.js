ccbc9bcda3f77bd0bd688108dccf6d3add61291e
Chestna <chestna.mitchell@outlook.com>
2024-02-08 12:27:24 -0500

    getting started on the face

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -5,6 +5,16 @@ function Nav() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
       <div className="container-fluid">
         <NavLink className="navbar-brand" to="/">CarCar</NavLink>
+        <NavLink className="navbar-brand" to="/api/manufacturers/">Manufacturers</NavLink>
+        <NavLink className="navbar-brand" to="/api/manufacturers/">Create a Manufacturer</NavLink>
+        <NavLink className="navbar-brand" to="/api/models/">Models</NavLink>
+        <NavLink className="navbar-brand" to="/api/models/">Models</NavLink>
+        <NavLink className="navbar-brand" to="/api/automobiles/">Automobiles</NavLink>
+        <NavLink className="navbar-brand" to="/api/automobiles/">Create an Automobile</NavLink>
+        <NavLink className="navbar-brand" to="/api/salespeople/">Salespeople</NavLink>
+        <NavLink className="navbar-brand" to="/api/salespeople/">Add a Salesperson</NavLink>
+        <NavLink className="navbar-brand" to="/api/customers/">Customers</NavLink>
+        
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
